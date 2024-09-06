adc40dea4302a7c5864b93a051c63982789017c7
Lutz <lutz.herrmann@mailbox.org>
2024-09-06 09:56:59 +0200

    eva

---
+++ 01_EVA_programme/taschenrechner_float/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Gib die Zahl a ein: ")
+	aString, _ := reader.ReadString('\n')
+	a, err := strconv.ParseFloat(strings.TrimSpace(aString), 64)
+	if err != nil {
+		fmt.Println("Fehler:", err)
+		return
+	}
+	fmt.Print("Gib die Zahl b ein: ")
+	bString, _ := reader.ReadString('\n')
+	b, err := strconv.ParseFloat(strings.TrimSpace(bString), 64)
+	if err != nil {
+		fmt.Println("Fehler:", err)
+		return
+	}
+	fmt.Println("Summe von a und b:", a+b)
+
+}
